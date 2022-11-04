c7eb94c81214e453b91e8c24af23bb2e7ca5e875
Aleecza <alexander.riquelmef@gmail.com>
2022-11-04 15:38:15 -0300

    [Epic] [Task] - Add DTO for task - Add DTO for Project (Incomplete) - Create task module

--- src/modules/project/dto/create-project.dto.ts
+++ src/modules/project/dto/create-project.dto.ts
@@ -1,1 +1,23 @@
-export class CreateProjectDto {}
+import { IsDate, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateProjectDto {
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
+  @MaxLength(255)
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  description: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  dateStart: Date;
+
+  @IsDate()
+  @IsNotEmpty()
+  dateEnd: Date;
+}
